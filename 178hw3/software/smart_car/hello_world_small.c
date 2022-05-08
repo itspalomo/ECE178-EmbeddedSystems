aebefb5586b3636c5af1fe720cd80489737b94ba
Jose Maciel <mrmaciman101@mail.fresnostate.edu>
2022-05-08 15:38:53 -0700

    gpio working

---
+++ 178hw3/software/smart_car/hello_world_small.c
@@ -0,0 +1,74 @@
+#include "system.h"
+#include "io.h"
+#include "sys/alt_stdio.h"
+#include "altera_avalon_pio_regs.h"
+#include "sys/alt_irq.h"
+#include "alt_types.h"
+#include "altera_avalon_timer_regs.h"
+
+#define HEXVAL_0 0b11000000 /*0*/
+#define HEXVAL_1 0b11111001 /*1*/
+#define HEXVAL_2 0b10100100 /*2*/
+#define HEXVAL_3 0b10110000 /*3*/
+#define HEXVAL_4 0b10011001 /*4*/
+#define HEXVAL_5 0b10010010 /*5*/
+#define HEXVAL_6 0b10000010 /*6*/
+#define HEXVAL_7 0b11111000 /*7*/
+#define HEXVAL_8 0b10000000 /*8*/
+#define HEXVAL_9 0b10011000 /*9*/
+#define HEXVAL_NEG 0b0111111 /*-*/
+#define HEXVAL_CLEAR 0b11111111 /*Clear*/
+
+int hextable[] = {HEXVAL_0, HEXVAL_1, HEXVAL_2, HEXVAL_3, HEXVAL_4,
+					HEXVAL_5, HEXVAL_6, HEXVAL_7, HEXVAL_8, HEXVAL_9};
+
+
+
+void clearhex();
+void decimal_to_hex(alt_u8);
+
+
+int main()
+{ 
+
+	alt_printf("Testing GPIO\n");
+	IOWR_ALTERA_AVALON_PIO_DATA(MOT_0_BASE,0b1111);
+	IOWR_ALTERA_AVALON_PIO_DATA(MOT_1_BASE,0b1111);
+
+	while(1){
+
+	}
+  return 0;
+}
+
+
+
+void clearhex()
+{
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_0_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_1_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_2_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_3_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_4_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_5_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_6_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_7_BASE,HEXVAL_CLEAR);
+}
+
+void decimal_to_hex(alt_u8 uhex)
+{
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_0_BASE, hextable[(uhex)%10]);
+	uhex /= 10;
+
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_1_BASE, hextable[(uhex)%10]);
+	uhex /= 10;
+
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_2_BASE, hextable[uhex%10]);
+
+
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_3_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_4_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_5_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_6_BASE,HEXVAL_CLEAR);
+	IOWR_ALTERA_AVALON_PIO_DATA(HEX_7_BASE,HEXVAL_CLEAR);
+}
---
+++ 178hw3/software/smart_car/obj/default/hello_world_small.d
@@ -0,0 +1,31 @@
+obj/default/hello_world_small.o: hello_world_small.c \
+ ../smart_car_bsp/system.h ../smart_car_bsp/linker.h \
+ ../smart_car_bsp//HAL/inc/io.h ../smart_car_bsp//HAL/inc/alt_types.h \
+ ../smart_car_bsp//HAL/inc/sys/alt_stdio.h \
+ ../smart_car_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../smart_car_bsp//HAL/inc/sys/alt_irq.h \
+ ../smart_car_bsp//HAL/inc/nios2.h ../smart_car_bsp//HAL/inc/alt_types.h \
+ ../smart_car_bsp//HAL/inc/priv/alt_legacy_irq.h \
+ ../smart_car_bsp//drivers/inc/altera_avalon_timer_regs.h
+
+../smart_car_bsp/system.h:
+
+../smart_car_bsp/linker.h:
+
+../smart_car_bsp//HAL/inc/io.h:
+
+../smart_car_bsp//HAL/inc/alt_types.h:
+
+../smart_car_bsp//HAL/inc/sys/alt_stdio.h:
+
+../smart_car_bsp//drivers/inc/altera_avalon_pio_regs.h:
+
+../smart_car_bsp//HAL/inc/sys/alt_irq.h:
+
+../smart_car_bsp//HAL/inc/nios2.h:
+
+../smart_car_bsp//HAL/inc/alt_types.h:
+
+../smart_car_bsp//HAL/inc/priv/alt_legacy_irq.h:
+
+../smart_car_bsp//drivers/inc/altera_avalon_timer_regs.h:
