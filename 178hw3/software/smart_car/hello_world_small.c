e216d8abe078e28cc5faf43fe4fc3be9e86b9ac7
Jose Maciel <mrmaciman101@mail.fresnostate.edu>
2022-05-08 17:42:35 -0700

    rs232 send data working

--- 178hw3/software/smart_car/hello_world_small.c
+++ 178hw3/software/smart_car/hello_world_small.c
@@ -5,6 +5,8 @@
 #include "sys/alt_irq.h"
 #include "alt_types.h"
 #include "altera_avalon_timer_regs.h"
+#include "altera_up_avalon_rs232.h"
+#include "altera_up_avalon_rs232_regs.h"
 
 #define HEXVAL_0 0b11000000 /*0*/
 #define HEXVAL_1 0b11111001 /*1*/
@@ -19,6 +21,8 @@
 #define HEXVAL_NEG 0b0111111 /*-*/
 #define HEXVAL_CLEAR 0b11111111 /*Clear*/
 
+#define FIFO_EMPTY 0x0
+
 int hextable[] = {HEXVAL_0, HEXVAL_1, HEXVAL_2, HEXVAL_3, HEXVAL_4,
 					HEXVAL_5, HEXVAL_6, HEXVAL_7, HEXVAL_8, HEXVAL_9};
 
@@ -27,21 +31,58 @@ int hextable[] = {HEXVAL_0, HEXVAL_1, HEXVAL_2, HEXVAL_3, HEXVAL_4,
 void clearhex();
 void decimal_to_hex(alt_u8);
 
+/*UART read bluetooth*/
+void taskRS232();
 
 int main()
-{ 
+{
 
 	alt_printf("Testing GPIO\n");
-	IOWR_ALTERA_AVALON_PIO_DATA(MOT_0_BASE,0b1111);
-	IOWR_ALTERA_AVALON_PIO_DATA(MOT_1_BASE,0b1111);
+	IOWR_ALTERA_AVALON_PIO_DATA(MOT_0_BASE,0b0000);
+	IOWR_ALTERA_AVALON_PIO_DATA(MOT_1_BASE,0b0000);
 
-	while(1){
+	taskRS232();
 
-	}
   return 0;
 }
 
+void taskRS232()
+{
+	alt_u32 FIFO_write;
+	alt_u16 FIFO_read;
+	char data_r8;
+	char data_w8;
+	unsigned p_error;
 
+	alt_up_rs232_dev* rs232dev;
+
+	/*Open serial port*/
+	rs232dev = alt_up_rs232_open_dev("/dev/uart");
+	if (rs232dev == NULL)
+		alt_printf("Error: could not open RS232 UART\n");
+	else
+		alt_printf("Opened RS232 UART device\n");
+
+	alt_up_rs232_enable_read_interrupt(rs232dev);
+
+	char temp;
+
+	alt_printf("To Read enter a, else will write");
+
+
+	while(1){
+
+		temp = alt_getchar();
+
+			alt_up_rs232_disable_read_interrupt(rs232dev);
+			data_w8 = 'a';
+			FIFO_write = alt_up_rs232_get_available_space_in_write_FIFO(rs232dev);
+			if(FIFO_write == 0x80){
+				alt_up_rs232_write_data(rs232dev,data_w8);
+				alt_printf("Wrote %c to RS232 UART", data_w8);
+			}
+	}
+}
 
 void clearhex()
 {
--- 178hw3/software/smart_car/obj/default/hello_world_small.d
+++ 178hw3/software/smart_car/obj/default/hello_world_small.d
@@ -6,7 +6,12 @@ obj/default/hello_world_small.o: hello_world_small.c \
  ../smart_car_bsp//HAL/inc/sys/alt_irq.h \
  ../smart_car_bsp//HAL/inc/nios2.h ../smart_car_bsp//HAL/inc/alt_types.h \
  ../smart_car_bsp//HAL/inc/priv/alt_legacy_irq.h \
- ../smart_car_bsp//drivers/inc/altera_avalon_timer_regs.h
+ ../smart_car_bsp//drivers/inc/altera_avalon_timer_regs.h \
+ ../smart_car_bsp//drivers/inc/altera_up_avalon_rs232.h \
+ ../smart_car_bsp//HAL/inc/sys/alt_dev.h \
+ ../smart_car_bsp//HAL/inc/sys/alt_llist.h \
+ ../smart_car_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../smart_car_bsp//drivers/inc/altera_up_avalon_rs232_regs.h
 
 ../smart_car_bsp/system.h:
 
@@ -29,3 +34,13 @@ obj/default/hello_world_small.o: hello_world_small.c \
 ../smart_car_bsp//HAL/inc/priv/alt_legacy_irq.h:
 
 ../smart_car_bsp//drivers/inc/altera_avalon_timer_regs.h:
+
+../smart_car_bsp//drivers/inc/altera_up_avalon_rs232.h:
+
+../smart_car_bsp//HAL/inc/sys/alt_dev.h:
+
+../smart_car_bsp//HAL/inc/sys/alt_llist.h:
+
+../smart_car_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../smart_car_bsp//drivers/inc/altera_up_avalon_rs232_regs.h:
